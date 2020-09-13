c29d69724b5341158e9d9c353aa5927c1a3442c7
kkirru <qorgusdn04@gmail.com>
2020-09-13 16:40:56 +0900

    init

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kkirru/Documents/Dev/CEOS/django-study/django-tutorial-12th/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
