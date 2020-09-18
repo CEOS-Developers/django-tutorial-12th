067ebaa912edb4b51c5fcf2d11e166b2f0e81da1
AlphaTechnic <58129950+AlphaTechnic@users.noreply.github.com>
2020-09-18 22:39:39 +0900

    settings.py를 약간 수정하였습니다. TIME_ZONE = 'Asia/Seoul' 설정

--- AlphaTechnic/mysite/mysite/settings.py
+++ AlphaTechnic/mysite/mysite/settings.py
@@ -120,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -133,3 +133,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
