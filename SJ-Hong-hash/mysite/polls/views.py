7a4ad76d9dd40b6e4e96ea04b9b41e6b3e437837
SJ-Hong-hash <seojoon.hong94@gmail.com>
2020-09-19 10:48:13 +0900

    render() done

--- SJ-Hong-hash/mysite/polls/views.py
+++ SJ-Hong-hash/mysite/polls/views.py
@@ -2,10 +2,23 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # polls/index.html에서 template을 불러온 후, 해당 context를 전달한다.
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+# render을 이용하면 index함수를 더 단순하게 할 수 있다.
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    context = {'latest_question_list': latest_question_list}
+#    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
